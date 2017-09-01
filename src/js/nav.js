81827a2e7057e3ac00c7e4fcad8e1b5482a89f24
uuit <1812890566@qq.com>
2017-09-01 21:32:56 +0800

    update photo css

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,19 +1,15 @@
 "use strict";
 let pathName = window.location.pathname;
 console.log(pathName);
-if(pathName.indexOf('detail')!==-1){
-  $("#detail").addClass("active");
-}else if(pathName.indexOf('list')!==-1){
-  $("#list").addClass("active");
-}else if(pathName.indexOf('about')!==-1){
-  $("#about").addClass("active");
+if(pathName.indexOf('photo')!==-1){
+  $("#photo").addClass("active");
 }else{
   $("#index").addClass("active");
 }
 
-if(pathName==="/"){
+/*if(pathName==="/"){
   $("body").addClass("bg-white");
 }else{
   $("body").addClass("bg-grey");
-}
+}*/
 

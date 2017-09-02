b36b66b8a048fcb31bbeed5a6cdb5c13e2269fb1
dszshen <1812890566@qq.com>
2017-09-02 23:10:00 +0800

    update

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,5 +1,5 @@
 "use strict";
-let pathName = window.location.pathname;
+var pathName = window.location.pathname;
 console.log(pathName);
 if(pathName.indexOf('photo')!==-1){
   $("#photo").addClass("active");

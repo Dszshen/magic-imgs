a3f6ee09e3cd97ead8c48b40634223a9cc3bb1f8
dszshen <1812890566@qq.com>
2017-09-02 12:43:36 +0800

    photo

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,6 +3,10 @@ let pathName = window.location.pathname;
 console.log(pathName);
 if(pathName.indexOf('photo')!==-1){
   $("#photo").addClass("active");
+}else if(pathName.indexOf('story')!==-1){
+  $("#story").addClass("active");
+}else if(pathName==="/"){
+  $("#index").addClass("active");
 }else{
   $("#index").addClass("active");
 }
